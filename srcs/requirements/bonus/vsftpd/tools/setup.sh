071e809c9519d2a3898eda0b497573c61154d3ad
Nikster3k <144370425+Nikster3k@users.noreply.github.com>
2024-09-02 15:58:12 +0200

    FIX: ftp server upload

--- srcs/requirements/bonus/vsftpd/tools/setup.sh
+++ srcs/requirements/bonus/vsftpd/tools/setup.sh
@@ -1,6 +1,8 @@
 useradd -m "$FTP_USER"
+chown $FTP_USER:$FTP_USER /home/$FTP_USER
+# chmod 755 /home/$FTP_USER
 echo "$FTP_USER:$FTP_PASS" | chpasswd
-
+# echo "local_root=/home/$FTP_USER" >> /etc/vsftpd.conf
 echo $FTP_USER > /etc/vsftpd.userlist
 
 exec systemctl start vsftpd
